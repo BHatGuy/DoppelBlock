e51961fd48233fd5b142e33093a7d4d704bf663b
Malte Müller <malte-m@gmx.net>
2019-04-02 10:51:45 +0200

    Crude layout for game input activity

--- app/src/main/java/com/malte_mueller/dokobo/GameInputActivity.java
+++ app/src/main/java/com/malte_mueller/dokobo/GameInputActivity.java
@@ -3,11 +3,11 @@ package com.malte_mueller.dokobo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class GameInputActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_gameinput);
     }
 }
