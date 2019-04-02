d7a70ef7c7d41cfb58193c5213cc81106a50c937
Malte Müller <malte-m@gmx.net>
2019-04-02 11:54:31 +0200

    Stubs for submit actions (game input)

--- app/src/main/java/com/malte_mueller/dokobo/GameInputActivity.java
+++ app/src/main/java/com/malte_mueller/dokobo/GameInputActivity.java
@@ -2,6 +2,11 @@ package com.malte_mueller.dokobo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class GameInputActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class GameInputActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameinput);
+
+        // your text box
+        EditText valueInput = (EditText) findViewById(R.id.eT_gameValue);
+
+        valueInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                Log.d("poo", v.getText().toString());
+                return false;
+            }
+        });
+    }
+
+    public void onGameSubmit(View v){
+        //TODO: implement
+
     }
 }
