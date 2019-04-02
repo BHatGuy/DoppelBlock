e1970731bf1fd83ca86b1248985434dd95d6c78a
Malte Müller <malte-m@gmx.net>
2019-04-02 12:18:00 +0200

    Add crude layout for table input activity

--- app/src/main/java/com/malte_mueller/dokobo/GameInputActivity.java
+++ app/src/main/java/com/malte_mueller/dokobo/GameInputActivity.java
@@ -13,7 +13,7 @@ public class GameInputActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gameinput);
+        setContentView(R.layout.activity_game_input);
 
         // your text box
         EditText valueInput = (EditText) findViewById(R.id.eT_gameValue);
